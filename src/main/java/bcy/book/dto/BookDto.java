6668494d4e8a23225ad6a204a63fbd0563d3b393
kimtaewoo <ktw2000111@naver.com>
2025-12-11 16:32:27 +0900

    책 검색기능 추가

---
+++ src/main/java/bcy/book/dto/BookDto.java
@@ -0,0 +1,23 @@
+package bcy.book.dto;
+
+import bcy.book.entity.BookEntity;
+
+public record BookDto(
+        String isbn,
+        String title,
+        String author,
+        String publisher,
+        String pubDate,
+        String image
+) {
+    public static BookDto from(BookEntity e) {
+        return new BookDto(
+                e.getIsbn(),
+                e.getTitle(),
+                e.getAuthor(),
+                e.getPublisher(),
+                e.getPubdate(),
+                e.getImage()
+        );
+    }
+}
